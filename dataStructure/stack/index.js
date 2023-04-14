92fea765ed2b2f33470ef242050eed6b309cf0db
iamzjt-front-end <iamzjt@foxmail.com>
2023-04-14 08:32:44 +0800

    🔥 feat: 03_栈.md

---
+++ dataStructure/stack/index.js
@@ -0,0 +1,40 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  clear() {
+    this.items = [];
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  print() {
+    console.log(this.items.toString());
+  }
+}
+
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.pop();
+stack.pop();
+console.log(stack);
