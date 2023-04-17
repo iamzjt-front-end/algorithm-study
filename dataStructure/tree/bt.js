23d75ec56921906642444b8d64d5f6379a72c436
iamzjt-front-end <iamzjt@foxmail.com>
2023-04-18 07:24:44 +0800

    📝 feat: 二叉树的先序遍历实现

---
+++ dataStructure/tree/bt.js
@@ -0,0 +1,30 @@
+// 二叉树
+export const bt = {
+	val: 'root',
+	left: {
+		val: 'left-1',
+		left: {
+			val: 'left-1-1',
+			left: null,
+			right: null
+		},
+		right: {
+			val: 'left-1-2',
+			left: null,
+			right: null
+		}
+	},
+	right: {
+		val: 'right-1',
+		left: {
+			val: 'right-1-1',
+			left: null,
+			right: null
+		},
+		right: {
+			val: 'right-1-2',
+			left: null,
+			right: null
+		}
+	}
+}
---
+++ dataStructure/tree/preorder.js
@@ -0,0 +1,19 @@
+// 二叉树的 先序遍历
+import { bt } from "./bt.js";
+
+// 根 -> 左 -> 右
+const preorder = (root) => {
+	if (!root) return;
+	console.log(root.val);
+	preorder(root.left);
+	preorder(root.right);
+};
+
+preorder(bt);
+
+// left-1
+// left-1-1
+// left-1-2
+// right-1
+// right-1-1
+// right-1-2
